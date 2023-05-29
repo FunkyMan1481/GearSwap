acd6539256f41b589262ca12e051966dee9402b2
adam ollivier <adamollivier96@gmail.com>
2023-05-29 16:25:58 +0200

    devise first tete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/first_name'
+  get 'users/last_name'
+  get 'users/email'
+  get 'users/password'
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
