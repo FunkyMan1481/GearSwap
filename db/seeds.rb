aff579a44e4332905ea6626a88666a7193fa6d89
stanislas zablocki <stanislas.zablocki@gmail.com>
2023-05-29 15:58:14 +0200

    seed MAJ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,36 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # db/seeds.rb
+# db/seeds.rb
 
-equipment_names = ['Ballon de football', 'Raquette de tennis', 'Maillot de bain', 'Casque de vélo', 'Chaussures de course',
-  'Sac de golf', 'Tapis de yoga', 'Gants de boxe', 'Balle de baseball', 'Pagaie de kayak', 'Chaussures de randonnée',
-  'Planche de surf', 'Ski', 'Batte de cricket', 'Frisbee', 'Masque de plongée', 'Balle de rugby', 'Baskets',
-  'Dumbbells', 'Corde à sauter', 'Ballon de basketball', 'Haltères', 'Bandes de résistance', 'Tapis de course',
-  'Ballon de volley', 'Equipement d\'escalade', 'Raquette de badminton', 'Bottes de ski', 'Casque de ski',
-  'Poids de cheville', 'Barre de traction', 'Gants de gardien', 'Planche à voile', 'Chaussures de danse',
-  'Équipement d\'équitation', 'Raquette de squash', 'Table de ping pong', 'Ballon de handball', 'Bâtons de randonnée',
-  'Gants de cyclisme', 'Équipement de plongée', 'Ballon de foot US', 'Arc de tir à l'arc', 'Balle de squash',
-  'Équipement de pêche', 'Équipement de gymnastique', 'Raquette de paddle', 'Kettlebell', 'Bâton de hockey',
-  'Équipement de ski de fond', 'Équipement de water polo']
+# ...
 
-categories = ['Football', 'Tennis', 'Natation', 'Cyclisme', 'Athlétisme', 'Golf', 'Yoga', 'Boxe', 'Baseball', 'Kayak',
-'Randonnée', 'Surf', 'Ski', 'Cricket', 'Frisbee', 'Plongée', 'Rugby', 'Basketball', 'Gym', 'Volleyball',
-'Escalade', 'Badminton', 'Course', 'Handball', 'Cyclisme', 'Danse', 'Équitation', 'Squash', 'Ping pong',
-'Randonnée', 'Foot US', 'Tir à l\'arc', 'Pêche', 'Gymnastique', 'Paddle', 'Hockey', 'Ski de fond', 'Water polo']
+# Liste d'équipements sportifs possibles
+equipment_names = [
+  'Ballon de football', 'Raquette de tennis', 'Casque de vélo', 'Chaussures de course',
+  'Tapis de yoga', 'Gants de boxe', 'Poids pour haltères', 'Barre de traction',
+  'Ballon de basketball', 'Bâton de hockey', 'Patin à glace', 'Planche de surf',
+  'Raquette de badminton', 'Ballon de volleyball', 'Gants de gardien de but', 'Maillot de cyclisme',
+  'Planche à voile', 'Combinaison de plongée', 'Gilet de sauvetage', 'Corde à sauter',
+  'Sac de frappe', 'Bande de résistance', 'Ballon de rugby', 'Raquette de ping-pong',
+  'Balle de golf', 'Club de golf', 'Tee de golf', 'Crampons de football',
+  'Protège-tibias', 'Casque de football', 'Masque de ski', 'Bâtons de ski',
+  'Chaussures de randonnée', 'Boussole', 'Sac à dos de randonnée', 'Tente de camping',
+  'Sac de couchage', 'Matelas gonflable', 'Lampe de poche', 'Gourde',
+  'Bâton de marche', 'Chaussures d’escalade', 'Harnais d’escalade', 'Corde d’escalade',
+  'Mousqueton', 'Descendeur', 'Magnésie', 'Casque d’escalade',
+  'Planche de skateboard', 'Roues de skateboard', 'Trucks de skateboard', 'Grip de skateboard',
+  'Ballon de handball', 'Maillot de bain', 'Lunettes de natation', 'Bonnet de natation'
+]
 
-50.times do
-Equipment.create!(
-name: equipment_names.sample,
-category: categories.sample,
-price: rand(10.0..100.0).round(2),
-quantity: rand(1..20)
-)
+# Création des équipements sportifs
+equipment_names.each do |name|
+  equipment = {
+    name: name,
+    category: 'Sport',
+    description: "#{name} de qualité supérieure",
+    available: [true, false].sample, # Disponibilité aléatoire
+    price: rand(10.0..100.0).round(2), # Prix aléatoire
+  }
+  Equipement.create!(equipment)
 end
