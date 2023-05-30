e7a7286c8762b9071eb19d9be15fb419c7c99fac
adam ollivier <adamollivier96@gmail.com>
2023-05-30 12:24:54 +0200

    ceci-tente d'êtreune home page devise

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_142118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-  
   create_table "equipements", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -40,6 +26,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_142118) do
     t.date "date_de_fin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
