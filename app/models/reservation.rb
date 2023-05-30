b5c68521e0d5e9a2a46d82c82ad1528eaaa809d6
adam ollivier <adamollivier96@gmail.com>
2023-05-30 16:16:57 +0200

    réglage prob

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
-  belongs_to :equipment
+  belongs_to :gear
 
   validates :start_date, presence: true
   validates :end_date, presence: true
