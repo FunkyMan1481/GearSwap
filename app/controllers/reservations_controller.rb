86d93529aade0ea4237f1e131766dbb4169ec8fe
guillaumebregaint <guillaume.bregaint1@gmail.com>
2023-05-30 11:56:30 +0200

    reservations done (view, route et controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'users/password'
   devise_for :users
   root to: "pages#home"
+  get '/reservations', to: 'reservations#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :equiments, only: %i[index new create]
   resources :reservations, only: %i[index new create]
