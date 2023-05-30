9fe0f07e9edc660096043d0d4885fe83f85df936
adam ollivier <adamollivier96@gmail.com>
2023-05-30 15:04:14 +0200

    bulle log avatar + profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'users/last_name'
   get 'users/email'
   get 'users/password'
+  get 'profile', to: 'pages#profile'
+  patch 'profile', to: 'pages#profile'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
